039576970db7c06294913d10567ca3a934032f84
manon-bernard <manon.bernard8907@gmail.com>
2023-01-22 19:43:37 +0100

    Added: Overall style for mobile sizes

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const mobileMenuToggle = document.querySelector('.mobile-nav__toggle');
-const nav = document.querySelector('.nav-list');
+const nav = document.querySelector('nav');
 const mobileMenuOpen = document.querySelector('.menu__icon--open');
 const mobileMenuClose = document.querySelector('.menu__icon--close');
 
@@ -7,6 +7,18 @@ mobileMenuToggle.addEventListener('click', handleMenuToggle);
 
 function handleMenuToggle(event){
     console.log(event);
+    nav.classList.toggle('visually-hidden');
+    mobileMenuOpen.classList.toggle('visually-hidden');
+    mobileMenuClose.classList.toggle('visually-hidden');
+};
+
+const navLinks = document.querySelectorAll('.nav__link');
+
+for (let navLink of navLinks) {
+    navLink.addEventListener('click', handleMenuLinks)
+};
+
+function handleMenuLinks(){
     nav.classList.toggle('visually-hidden');
     mobileMenuOpen.classList.toggle('visually-hidden');
     mobileMenuClose.classList.toggle('visually-hidden');
